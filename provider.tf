d885598ca713bb09ae9bc4d07e4849f13a53b1ea
davidbenja87 <38088886+davidbenja87@users.noreply.github.com>
2021-04-24 08:48:10 +0100

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -22,7 +22,7 @@ terraform {
 provider "azurerm" {
   features {}
 
-  subscription_id = GITHUB_AZ_SUB
+  subscription_id = var.github_az_sub
     client_id       = "f30d30cf-bf20-4b62-8192-eb4019f35ad1"
     client_secret   = "iyIeh.i_KapdTX7YW5f3xXNv-2.K8Pwz5l"
     tenant_id       = "c8fedfb4-d804-457b-9745-362581de2e4c"
