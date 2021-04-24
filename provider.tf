1cd7bb867bb5aa679b48d9d91534a0364c1dc4d6
Sugumar <sugumarb@b2b.regn.net>
2021-04-24 06:48:44 +0100

    my first commit

--- provider.tf
+++ provider.tf
@@ -1,7 +1,7 @@
 
 
 provider "azurerm" {
-    version = "=1.42"
+    version = "=2.46.0"
     subscription_id = "974668b8-821d-4cc2-a84d-c81a7733f464"
     client_id       = "f30d30cf-bf20-4b62-8192-eb4019f35ad1"
     client_secret   = "iyIeh.i_KapdTX7YW5f3xXNv-2.K8Pwz5l"
