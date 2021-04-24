d7b8ad11627370b2092cd6ad48db13f9de468861
Sugumar <sugumarb@b2b.regn.net>
2021-04-24 06:46:37 +0100

    my first commit

---
+++ .github/workflows/resourcegrp.tf
@@ -0,0 +1,10 @@
+resource "azurerm_resource_group" "purview_rg" {
+  name     = "bds-dev-pur-rg"
+  location = "Southeast Asia"
+  
+    tags = {
+    environment = "dev"
+	description = "BDS dev resource group"
+  }
+
+}
---
+++ provider.tf
@@ -0,0 +1,9 @@
+
+
+provider "azurerm" {
+    version = "=1.42"
+    subscription_id = "974668b8-821d-4cc2-a84d-c81a7733f464"
+    client_id       = "f30d30cf-bf20-4b62-8192-eb4019f35ad1"
+    client_secret   = "iyIeh.i_KapdTX7YW5f3xXNv-2.K8Pwz5l"
+    tenant_id       = "c8fedfb4-d804-457b-9745-362581de2e4c"
+}
