64521b921196b12eab2de3861e11fc7d01679698
Sugumar <sugumarb@b2b.regn.net>
2021-04-25 06:03:08 +0100

    Remove rgp

--- resourcegrp.tf
+++ resourcegrp.tf
@@ -1,11 +1,11 @@
-#resource "azurerm_resource_group" "purview_rg" {
-#  name     = "bds-dev-pur-rg"
-#  location = "Southeast Asia"
-#  
-#    tags = {
-#    environment = "dev"
-#	description = "BDS dev resource group"
-#  }
-#
-#
-#}
+resource "azurerm_resource_group" "purview_rg" {
+  name     = "bds-dev-pur-rg"
+  location = "Southeast Asia"
+  
+    tags = {
+    environment = "dev"
+	description = "BDS dev resource group"
+  }
+
+
+}
